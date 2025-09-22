727f43a0c162abc1b6b1bd599b5b733e046bb3cf
SahilKumar78 <sahilabcd661@gmail.com>
2025-09-22 17:55:17 +0530

    Added the database

--- backend/database/db.js
+++ backend/database/db.js
@@ -5,7 +5,9 @@ exports.connectToDB=async()=>{
     try {
         await mongoose.connect(process.env.MONGO_URI)
         console.log('connected to DB');
+        console.log(process.env.MONGO_URI)
     } catch (error) {
-        console.log(error);
+        console.log(error?.message);
+
     }
 }
